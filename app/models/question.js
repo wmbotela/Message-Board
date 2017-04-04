8517d10052e0e53bd9663c013751bad3aefc31ea
wmbotela <wmbotela@gmail.com>
2017-04-04 03:54:07 +0300

    After merge
    Commit devoid of errors resulting from merginging branches

--- app/models/question.js
+++ app/models/question.js
@@ -1,15 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-<<<<<<< HEAD
 
-  owner: DS.attr(),
-  image: DS.attr(),
-  answers: DS.hasMany('answer', { async: true })
 
-=======
   question: DS.attr(),
   author: DS.attr(),
->>>>>>> gh-pages
+
+
 
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,20 +1,9 @@
 import Ember from 'ember';
 
-  var questions = [{
-    owner:"William",
-    image: "https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg"
-  }];
-
 export default Ember.Route.extend({
-<<<<<<< HEAD
-  model() {
-    return questions;
-  },
-
-=======
 
   model() {
     return this.store.findAll('question');
   },
->>>>>>> gh-pages
+
 });
