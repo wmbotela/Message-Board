d89a9bf3404677af4fcb19d7f186bad833c7e996
wmbotela <wmbotela@gmail.com>
2017-04-05 04:32:40 +0300

    New Answer Tab
    This commit adds the new asnwer tab to our app

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model() {
-    return this.store.findAll('question');
-    return this.store.findAll('answer');
+    return Ember.RSVP.hash({
+    questions: this.store.findAll('question'),
+    answers: this.store.findAll('answer')
+    });
   },
 
   action: {
@@ -13,7 +15,11 @@ export default Ember.Route.extend({
       newQuestion.save();
       this.transitionTo('index');
     },
-
+    saveAnswer(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      newAnswer.save();
+      this.transitionTo('index');
+    }
 
   }
 
